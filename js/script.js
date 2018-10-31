bd447a167b6f36782b01e9799c468aad4e6a2076
Erusebiru <Erusebiru@gmail.com>
2018-10-31 16:39:15 +0100

    Solucion a bug de comerse a si mismo

--- js/script.js
+++ js/script.js
@@ -62,14 +62,67 @@ function calcularMovimientos(piezaSeleccionada){
 			var basey = base[i].getAttribute("posicionY");
 			var basex = base[i].getAttribute("posicionX");
 			var fichaEnemiga = base[i].getAttribute("color");
-			if(basey == posy || basex == posx){
-				var coordenadas = [basex,basey];
-				
-				
-				movimientos.push(coordenadas);
-				
+			if(!(basey == posy && basex == posx)){
+				if(basey == posy || basex == posx){
+					var coordenadas = [basex,basey];
+					if(base[i].getAttribute("name") != ""){
+
+					}
+					
+					movimientos.push(coordenadas);
+					
+				}
 			}
 		}
 		return movimientos;
 	}
 }
+
+/*function calcularMovimientos(piezaSeleccionada){
+	var pieza = piezaSeleccionada.getAttribute("name");
+	var movimientos = [];
+	if(pieza.toLowerCase().includes("torre")){
+		var posx = piezaSeleccionada.getAttribute("posicionx");
+		var posy = piezaSeleccionada.getAttribute("posiciony");
+		var ownColor = piezaSeleccionada.getAttribute("color");
+
+		var blockY = false;
+		var blockX = false;
+		for(var i=0;i<base.length;i++){
+			var control = true;
+			var basey = base[i].getAttribute("posicionY");
+			var basex = base[i].getAttribute("posicionX");
+			var fichaEnemiga = base[i].getAttribute("color");
+			if(!(basey == posy && basex == posx)){
+				if(basey == posy){
+					
+					if(base[i].getAttribute("name") != ""){
+						blockY = true;
+					}
+				}else{
+					blockY = false;
+				}
+
+				if(basex == posx){
+					
+					if(base[i].getAttribute("name") != ""){
+						blockX = true;
+					}
+					
+				}else{
+					blockX = false;
+				}
+
+				if(!blockX || !blockY){
+					var coordenadas = [basex,basey];
+					movimientos.push(coordenadas);
+				}
+	
+			}
+			
+				
+		}
+		return movimientos;
+	}
+}
+*/
